713179a72fb5d4b14dcc51fd1e9c7a3692fb4668
SMD_ASY <andrii.sydorov@googlemail.com>
2021-10-28 10:59:23 +0300

    The stage 6 project BullsAndCows was done

--- BullsAndCows/Stage06/Main.java
+++ BullsAndCows/Stage06/Main.java
@@ -5,6 +5,57 @@ import java.util.Random;
 import java.util.List;
 import java.util.ArrayList;
 
+/**
+ * Description
+ * 
+ * Some players need a challenge, so let's make the secret code in the game
+ * harder to guess. Add support for more than 10 symbols by adding letters. Now,
+ * the secret code can contain the numbers 0-9 and the lowercase Latin
+ * characters a-z. The new maximum length for the code is 36. Note that the
+ * length of the secret word may not match the number of possible characters in
+ * the secret code, so you should request t input twice: once for the secret
+ * code length and once for the number of possible characters.
+ * 
+ * Also, since a secret code is not a number anymore, the symbol 0 should be
+ * allowed as the first character in a secret code. 
+ * 
+ * Objectives
+ * 
+ * In this step, your program should:
+ * 
+ * 1. Ask for the length of the secret code. 
+ * 2. Ask for the range of possible characters in the secret code.
+ * 3. Generate a secret code using numbers and characters. This time, you should also print the secret code using *
+ * characters and print which characters were used to generate the secret code.
+ * 4. Function as a fully playable game.
+ * 
+ * Example
+ * 
+ * The greater-than symbol followed by a space > represents the user input. Note
+ * that it's not part of the input.
+ * 
+ * Input the length of the secret code: 
+ * > 4 
+ * Input the number of possible symbols in the code: 
+ * > 16 
+ * The secret is prepared: **** (0-9, a-f). 
+ * Okay, let's start a game! 
+ * Turn 1: 
+ * > 1a34 
+ * Grade: 1 bull and 1 cow 
+ * Turn 2:
+ * > b354 
+ * Grade: 2 bulls and 1 cow 
+ * Turn 3: 
+ * > 93b4 
+ * Grade: 4 bulls 
+ * 
+ * Congratulations! You guessed the secret code.
+ * 
+ * @author ASY
+ *
+ */
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -23,7 +74,6 @@ public class Main {
 		private int countCows;
 		private String secretNumber;
 		private int lengthOfSecretNumber;
-		private List<Integer> ls = new ArrayList<>();
 		private int numberOfPossibleSymbols;
 
 		static {
@@ -32,12 +82,9 @@ public class Main {
 				sb.append(Character.forDigit(i, 10));
 			}
 			for (int i = 'a'; i <= 'z'; i++) {
-				sb.append((char)i);
+				sb.append((char) i);
 			}
 			possibleSymbols = sb.toString();
-		}
-
-		public Game() {
 		}
 
 		private void createPlayers() {
@@ -74,6 +121,10 @@ public class Main {
 			do {
 				System.out.println("Turn " + index + " :");
 				userNumber = humanPlayer.getUserVariants();
+				if (userNumber.length() != secretNumber.length()) {
+					System.out.println("Invalid data length!");
+					return;
+				}
 				countAnimals(userNumber);
 				System.out.println("Grade: " + printResult() + "\n");
 				index++;
@@ -93,7 +144,7 @@ public class Main {
 									? possibleSymbols.charAt(0) + "-" + possibleSymbols.charAt(numberOfPossibleSymbols)
 									: possibleSymbols.charAt(0) + "-" + possibleSymbols.charAt(9);
 			String characterPart = numberOfPossibleSymbols < 10 ? ""
-					: possibleSymbols.charAt(10) + "-" + possibleSymbols.charAt(numberOfPossibleSymbols);
+					: possibleSymbols.charAt(10) + "-" + possibleSymbols.charAt(numberOfPossibleSymbols - 1);
 			String area = characterPart.isEmpty() ? "(" + digitPart + ")" : "(" + digitPart + "," + characterPart + ")";
 			System.out.println("The secret is prepared: " + stars + " " + area);
 			System.out.println("Okay, let's start a game!");
@@ -110,16 +161,17 @@ public class Main {
 		private void countAnimals(String userNumber) {
 			countBulls = 0;
 			countCows = 0;
+			List<String> ls = new ArrayList<>();
 			for (int i = 0; i < secretNumber.length(); i++) {
 				if (secretNumber.charAt(i) == userNumber.charAt(i)) {
 					countBulls++;
-					ls.add(Character.getNumericValue(userNumber.charAt(i)));
-					continue;
+					ls.add(Character.toString(userNumber.charAt(i)));
 				}
 			}
 			for (int i = 0; i < secretNumber.length(); i++) {
 				if (secretNumber.contains(Character.toString(userNumber.charAt(i)))
-						&& !ls.contains(Character.getNumericValue(userNumber.charAt(i)))) {
+						&& !ls.contains(Character.toString(userNumber.charAt(i)))) {
+					ls.add(Character.toString(userNumber.charAt(i)));
 					countCows++;
 				}
 			}
@@ -163,7 +215,7 @@ public class Main {
 					ls.add(temp);
 				}
 			} while (ls.size() != g.lengthOfSecretNumber);
-			return ls.toString();
+			return ls.toString().replaceAll("[^0-9a-z]", "");
 		}
 
 	}
@@ -172,7 +224,6 @@ public class Main {
 
 		private String lengthOfSecretNumber;
 		private String numberOfSymbols;
-		private String userVariants;
 		Game g;
 
 		public HumanPlayer(Game g) {
@@ -188,8 +239,7 @@ public class Main {
 		}
 
 		private String getUserVariants() {
-			userVariants = g.sc.nextLine();
-			return userVariants;
+			return g.sc.nextLine();
 		}
 	}
 

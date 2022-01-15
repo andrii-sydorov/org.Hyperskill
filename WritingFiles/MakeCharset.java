21fc2ebce456a3fbf0b566cc56693ad402167e5b
SMD_ASY <andrii.sydorov@googlemail.com>
2022-01-15 15:52:56 +0200

    The new topic was started

---
+++ WritingFiles/MakeCharset.java
@@ -0,0 +1,26 @@
+package WritingFiles;
+
+import java.nio.charset.Charset;
+import java.io.File;
+import java.io.FileReader;
+import java.io.PrintWriter;
+import java.io.IOException;
+
+public class MakeCharset {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Charset ch = Charset.forName("UTF-16");
+		ch.aliases().forEach(x -> System.out.println(x));
+		File f = new File("./src/WritingFiles/data.txt");
+
+		try (PrintWriter fw = new PrintWriter(f); FileReader fr = new FileReader(f);) {
+			fw.printf("%s dolor sit %s", "Lorem", "ipsum", "amet");
+			System.out.println(fr.read());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Working Directory = " + System.getProperty("user.dir"));
+	}
+
+}

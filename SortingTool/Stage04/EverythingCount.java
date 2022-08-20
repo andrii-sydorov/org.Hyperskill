312eb65f028c1f544d685452736d20c0c755d125
SMD_ASY <andrii.sydorov@googlemail.com>
2022-08-20 22:02:40 +0300

    The Stage04 of project SortingTool was done

--- SortingTool/Stage04/EverythingCount.java
+++ SortingTool/Stage04/EverythingCount.java
@@ -1,14 +1,114 @@
 package SortingTool.Stage04;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Scanner;
+
+/**
+ * Stage 4/6: Everything counts
+ * Description
+ * Now that it’s possible to sort numbers, it's time to implement the same
+ * functionality for words and lines. But that's not all for this stage! Let's
+ * also add a new sorting type that is often rather useful: sorting by count, a
+ * type of sorting that arranges the elements by number of occurrences.
+ * 
+ * Instead of -sortIntegers, we will use the universal -sortingType argument.
+ * 
+ * The result of sorting by count should be pairs (count, dataEntry) sorted by
+ * the count value.
+ * 
+ * Note that from this stage on, your program focuses on sorting user data, so
+ * it will no longer output information about the greatest number or the longest
+ * string.
+ * 
+ * Objectives
+ * Update the parsing of command-line arguments to support the sorting type
+ * definition:
+ * 
+ * - if the -sortingType argument is provided, it should be followed by natural or
+ * byCount, which defines the sorting type.
+ * - if the argument is not provided, then consider natural to be the default
+ * sorting type.
+ * Implement natural sorting for words and lines, and sorting by count for all
+ * data types. Within the group, elements with equal count values should be
+ * sorted naturally.
+ * 
+ * Note: for strings (words and lines), natural order is lexicographic order,
+ * for numbers it is numeric order.
+ * 
+ * Print the line containing the total number m of elements in the input: Total
+ * ELEMENTS: m., where ELEMENTS can be numbers, words, or lines, depending on
+ * the input. Then output the sorting results:
+ * 
+ * - for natural sorting of numbers or words, print elements on one line in
+ * ascending order:
+ * - Sorted data: el_1 el_2 ... el_m
+ * - for natural sorting of lines, print lexicographically sorted elements each on
+ * a new line:
+ * - Sorted data:
+ *   line_1
+ *   line_2
+ *   …
+ *   line_m
+ * - for sorting by count, print elements sorted by the number of occurrences,
+ * each on a new line, using the following format:
+ * element: count time(s), percentage%
+ * Here, element is a number, word, or line, count is the number of times it
+ * appears in the input, and percentage is the proportion of the count to the
+ * total number of elements in the input, given as a percentage.
+ * Examples
+ * Run configuration examples:
+ * 
+ * java SortingTool -sortingType natural -dataType long
+ * java SortingTool -dataType word -sortingType byCount
+ * Run examples
+ * 
+ * The greater-than symbol followed by a space (> ) represents the user input.
+ * Note that it's not part of the input.
+ * 
+ * Example 1, for sorting longs by count:
+ * 
+ * > 1 -2 33 4
+ * > 42
+ * > 1                1
+ * Total numbers: 7.
+ * -2: 1 time(s), 14%
+ * 4: 1 time(s), 14%
+ * 33: 1 time(s), 14%
+ * 42: 1 time(s), 14%
+ * 1: 3 time(s), 43%
+ * 
+ * Example 2, for sorting numbers naturally:
+ * 
+ * > 1 -2 33 4
+ * > 42
+ * > 1                1
+ * Total numbers: 7.
+ * Sorted data: -2 1 1 1 4 33 42
+ * 
+ * Example 3, for sorting words naturally:
+ * 
+ * > 1 -2 33 4
+ * > 42
+ * > 1                1
+ * Total words: 7.
+ * Sorted data: -2 1 1 1 33 4 42
+ * 
+ * Example 4, for sorting lines naturally:
+ * 
+ * > 1 -2 33 4
+ * > 42
+ * > 1                1
+ * Total lines: 3
+ * Sorted data:
+ * 1 1
+ * 1 -2 33 4
+ * 42
+ */
 
 public class EverythingCount {
 	private static String dataType;
@@ -25,45 +125,6 @@ public class EverythingCount {
 		buildAssociateMap();
 		buildDataToAnalyze(sc);
 		sc.close();
-	}
-
-	private static void sortIntegersAndBuildAnalytycs(Scanner sc) {
-		List<Integer> ls = new ArrayList<>();
-		while (sc.hasNextInt()) {
-			ls.add(sc.nextInt());
-		}
-		System.out.printf("Total numbers: %d\n", ls.size());
-		System.out.print("Sorted data: ");
-		ls.sort((x, y) -> x.compareTo(y));
-		ls.forEach(x -> System.out.print(x + " "));
-	}
-
-	private static int arrayContains(String[] args, String value) {
-		// implementing binary Search for Array
-		if (args.length == 0) {
-			return -1;
-		}
-		Arrays.sort(args);
-		int leftPointer = 0;
-		int rightPointer = args.length - 1;
-		int middle = 0;
-
-		while (leftPointer <= rightPointer) {
-
-			middle = (leftPointer + rightPointer) / 2;
-			if (args[middle].compareTo(value) > 0) {
-				rightPointer = middle - 1;
-			} else if (args[middle].compareTo(value) < 0) {
-				leftPointer = middle + 1;
-			}
-
-			if (args[middle].compareTo(value) == 0) {
-				return middle;
-			}
-
-		}
-		return -1;
-
 	}
 
 	private static void buildAssociateMap() {
@@ -85,21 +146,21 @@ public class EverythingCount {
 
 	private static void buildDataToAnalyze(Scanner sc) {
 		switch (dataType) {
-		case "long":
-			workWithLong(sc);
-			buildStatisticLongAndWord(dataType);
-			break;
-		case "word":
-			workWithWord(sc);
-			buildStatisticLongAndWord(dataType);
-			break;
-		case "line":
-			workWithLine(sc);
-			//buildStatisticWithLine(dataType);
-			break;
-		default:
-			System.out.println("Incorrect command line argumetns");
-			break;
+			case "long":
+				workWithLong(sc);
+				buildStatisticWithLong(dataType);
+				break;
+			case "word":
+				workWithWord(sc);
+				buildStatisticWithWord(dataType);
+				break;
+			case "line":
+				workWithLine(sc);
+				buildStatisticWithLine(dataType);
+				break;
+			default:
+				System.out.println("Incorrect command line argumetns");
+				break;
 		}
 	}
 
@@ -112,29 +173,54 @@ public class EverythingCount {
 		}
 	}
 
-	private static void buildStatisticLongAndWord(String dataType) {
+	private static void buildStatisticWithLong(String dataType) {
 		System.out.printf("Total %s: %d.\n", associateMap.get(dataType), count);
 		switch (kindOfComparator) {
-		case "natural":
-			//List<String> lsString = new ArrayList<String>(dataToAnalyze.keySet());
-			dataInput.sort((x, y) -> Integer.valueOf(x).compareTo(Integer.valueOf(y)));
-			System.out.print("Sorted data: ");
-			dataInput.forEach(x -> System.out.print(x + " "));
-			break;
-		case "byCount":
-			List<Entry<String, Integer>> list = new ArrayList<>(dataToAnalyze.entrySet());
-			Comparator<Entry<String, Integer>> cmp1 = (x, y) -> x.getValue().compareTo(y.getValue());
-			Comparator<Entry<String, Integer>> cmp2 = (x, y) -> Integer.valueOf(x.getKey())
-					.compareTo(Integer.valueOf(y.getKey()));
-			list.sort(cmp1.thenComparing(cmp2));
-			// print result
-			for (Entry<String, Integer> entr : list) {
-				System.out.printf("%s: %d time(s), %d%%\n", entr.getKey(), entr.getValue(),
-						entr.getValue() * 100 / count);
-			}
-			break;
-		default:
-			break;
+			case "natural":
+				// List<String> lsString = new ArrayList<String>(dataToAnalyze.keySet());
+				dataInput.sort((x, y) -> Integer.valueOf(x).compareTo(Integer.valueOf(y)));
+				System.out.print("Sorted data: ");
+				dataInput.forEach(x -> System.out.print(x + " "));
+				break;
+			case "byCount":
+				List<Entry<String, Integer>> list = new ArrayList<>(dataToAnalyze.entrySet());
+				Comparator<Entry<String, Integer>> cmp1 = (x, y) -> x.getValue().compareTo(y.getValue());
+				Comparator<Entry<String, Integer>> cmp2 = (x, y) -> Integer.valueOf(x.getKey())
+						.compareTo(Integer.valueOf(y.getKey()));
+				list.sort(cmp1.thenComparing(cmp2));
+				// print result
+				for (Entry<String, Integer> entr : list) {
+					System.out.printf("%s: %d time(s), %d%%\n", entr.getKey(), entr.getValue(),
+							entr.getValue() * 100 / count);
+				}
+				break;
+			default:
+				break;
+		}
+	}
+
+	private static void buildStatisticWithWord(String dataType) {
+		System.out.printf("Total %s: %d.\n", associateMap.get(dataType), count);
+		switch (kindOfComparator) {
+			case "natural":
+				// List<String> lsString = new ArrayList<String>(dataToAnalyze.keySet());
+				dataInput.sort((x, y) -> Integer.valueOf(x).compareTo(Integer.valueOf(y)));
+				System.out.print("Sorted data: ");
+				dataInput.forEach(x -> System.out.print(x + " "));
+				break;
+			case "byCount":
+				List<Entry<String, Integer>> list = new ArrayList<>(dataToAnalyze.entrySet());
+				Comparator<Entry<String, Integer>> cmp1 = (x, y) -> x.getValue().compareTo(y.getValue());
+				Comparator<Entry<String, Integer>> cmp2 = (x, y) -> x.getKey().compareTo(y.getKey());
+				list.sort(cmp1.thenComparing(cmp2));
+				// print result
+				for (Entry<String, Integer> entr : list) {
+					System.out.printf("%s: %d time(s), %d%%\n", entr.getKey(), entr.getValue(),
+							entr.getValue() * 100 / count);
+				}
+				break;
+			default:
+				break;
 		}
 	}
 
@@ -153,21 +239,6 @@ public class EverythingCount {
 		}
 	}
 
-	private static void buildStatisticWithWord(String dataType) {
-		System.out.printf("Total %s: %d.\n", associateMap.get(dataType), count);
-		Integer max = Integer.MIN_VALUE;
-		String key = null;
-		for (Map.Entry<String, Integer> entr : dataToAnalyze.entrySet()) {
-			if (entr.getKey().length() > max) {
-				max = entr.getKey().length();
-				key = entr.getKey();
-			}
-		}
-		System.out.printf("The longest %s: %s (%d time(s), %d%%).\n", dataType, key, dataToAnalyze.get(key),
-				dataToAnalyze.get(key) * 100 / count);
-
-	}
-
 	private static void workWithLine(Scanner sc) {
 		while (sc.hasNextLine()) {
 			String data = sc.nextLine();
@@ -183,16 +254,26 @@ public class EverythingCount {
 
 	private static void buildStatisticWithLine(String dataType) {
 		System.out.printf("Total %s: %d.\n", associateMap.get(dataType), count);
-		Integer max = Integer.MIN_VALUE;
-		String key = null;
-		for (Map.Entry<String, Integer> entr : dataToAnalyze.entrySet()) {
-			if (entr.getKey().length() > max) {
-				max = entr.getKey().length();
-				key = entr.getKey();
-			}
+		switch (kindOfComparator) {
+			case "natural":
+				// List<String> lsString = new ArrayList<String>(dataToAnalyze.keySet());
+				dataInput.sort((x, y) -> x.compareTo(y));
+				System.out.println("Sorted data:");
+				dataInput.forEach(x -> System.out.println(x));
+				break;
+			case "byCount":
+				List<Entry<String, Integer>> list = new ArrayList<>(dataToAnalyze.entrySet());
+				Comparator<Entry<String, Integer>> cmp1 = (x, y) -> x.getValue().compareTo(y.getValue());
+				Comparator<Entry<String, Integer>> cmp2 = (x, y) -> x.getKey().compareTo(y.getKey());
+				list.sort(cmp1.thenComparing(cmp2));
+				// print result
+				for (Entry<String, Integer> entr : list) {
+					System.out.printf("%s: %d time(s), %d%%\n", entr.getKey(), entr.getValue(),
+							entr.getValue() * 100 / count);
+				}
+				break;
+			default:
+				break;
 		}
-		System.out.printf("The longest %s:\n%s\n(%d time(s), %d%%).\n", dataType, key, dataToAnalyze.get(key),
-				dataToAnalyze.get(key) * 100 / count);
-
 	}
 }

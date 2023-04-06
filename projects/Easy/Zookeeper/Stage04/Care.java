3d59bca2e3ab78aafae8592ed027fcfe9a5bed9c
SMD_ASY <andrii.sydorov@googlemail.com>
2023-04-06 16:55:51 +0300

    The Stage04 of the project Zookeeper was done.

--- projects/Easy/Zookeeper/Stage04/Care.java
+++ projects/Easy/Zookeeper/Stage04/Care.java
@@ -1,85 +1,68 @@
-package projects.Easy.Zookeeper.Stage03;
+package projects.Easy.Zookeeper.Stage04;
 
 import java.util.Scanner;
 
 /**
- * Stage 3/4: 
- * 
- * What's inside?
+ * Stage 4/4: Sustainable care <3
  * 
  * Description
  * 
-The third stage requires you to increase the capabilities of your software. Now it should be able to recognize the number of a specific habitat from the input and show the animals living there.
+ * Now it's time to make our project user-friendly. In this final stage, you'll make your software ready for the zoo staff to use. Your program should understand the habitat numbers, show the animals, and be able to work continuously without having to be restarted.
+ * 
+ * Objectives
+ * 
+ * Your tasks at this point:
+ * Your program should repeat the behavior from the previous stage, but now in a loop.
+ * Do not forget to include an exit opportunity: inputting exit should end the program.
+ * When the program is done running, it should print: See you later!
+ * 
+ * Examples
+ * The greater-than symbol followed by a space (> ) represents the user input. Note that it's not part of the input.
+ * The final version of the program should run like this:
+ * 
+ * Example 1
+ * 
+ * Please enter the number of the habitat you would like to view: > 3
+ * 
+ * Switching on the camera in the goose habitat...
 
-Add all of the variables from the template to a single variable with the list type. The order of variables matters: they must appear on the list in the order in which they're defined in the code. The list must contain all of the variables with no duplicates.
+                                    _
+                                ,-"" "".
+                              ,'  ____  `.
+                            ,'  ,'    `.  `._
+   (`.         _..--.._   ,'  ,'        \    \
+  (`-.\    .-""        ""'   /          (  d _b
+ (`._  `-"" ,._             (            `-(   \
+ <_  `     (  <`<            \              `-._\
+  <`-       (__< <           :
+   (__        (_<_<          ;
+    `------------------------------------------
+The goose is staring intently at you... Maybe it's time to change the channel?
+Please enter the number of the habitat you would like to view: > 1
 
-Objectives
-
-In this stage your program should:
-
-Ask for the number of the desired habitat using the following phrase: Please enter the number of the habitat you would like to view:
-
-Use the input number as an index of your habitats to print its content.
-
-End with the following phrase:
-
----
-You've reached the end of the program. To check another habitat, please restart the watcher.
-Examples
-The greater-than symbol followed by a space (> ) represents the user input. Notice that it's not part of the input.
-
-Example 1
-
-Please enter the number of the habitat you would like to view: > 5
-
-Switching on the camera in the rabbit habitat...
-         ,
-        /|      __
-       / |   ,-~ /
-      Y :|  //  /
-      | jj /( .^
-      >-"~"-v"
-     /       Y
-    jo  o    |
-   ( ~T~     j
-    >._-' _./
-   /   "~"  |
-  Y     _,  |
- /| ;-"~ _  l
-/ l/ ,-"~    \
-\//\/      .- \
- Y        /    Y
- l       I     !
- ]\      _\    /"\
-(" ~----( ~   Y.  )
-It looks like we will soon have more rabbits!
----
-You've reached the end of the program. To check another habitat, please restart the watcher.
-Example 2
-
-Please enter the number of the habitat you would like to view: > 4
-
-Switching on the camera in the bat habitat...
-_________________               _________________
- ~-.              \  |\___/|  /              .-~
-     ~-.           \ / o o \ /           .-~
-        >           \\  W  //           <
-       /             /~---~\             \
-      /_            |       |            _\
-         ~-.        |       |        .-~
-            ;        \     /        i
-           /___      /\   /\      ___\
-                ~-. /  \_/  \ .-~
-                   V         V
-This bat looks like it's doing fine.
----
-You've reached the end of the program. To check another habitat, please restart the watcher.
+Switching on the camera in the lion habitat...
+                                               ,w.
+                                             ,YWMMw  ,M  ,
+                        _.---.._   __..---._.'MMMMMw,wMWmW,
+                   _.-""        '''           YP"WMMMMMMMMMb,
+                .-' __.'                   .'     MMMMW^WMMMM;
+    _,        .'.-'"; `,       /`     .--""      :MMM[==MWMW^;
+ ,mM^"     ,-'.'   /   ;      ;      /   ,       MMMMb_wMW"  @\
+,MM:.    .'.-'   .'     ;     `\    ;     `,     MMMMMMMW `"=./`-,
+WMMm__,-'.'     /      _.\      F'''-+,,   ;_,_.dMMMMMMMM[,_ / `=_}
+"^MP__.-'    ,-' _.--""   `-,   ;       \  ; ;MMMMMMMMMMW^``; __|
+           /   .'            ; ;         )  )`{  \ `"^W^`,   \  :
+          /  .'             /  (       .'  /     Ww._     `.  `"
+         /  Y,              `,  `-,=,_{   ;      MMMP`""-,  `-._.-,
+        (--, )                `,_ / `) \/"")      ^"      `-, -;"\:
+The lion is roaring!
+Please enter the number of the habitat you would like to view: > exit
+See you later!
  * @author SMD_ASY
  *
  */
 
-public class Inside {
-
+public class Care {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String camel = """
@@ -204,13 +187,19 @@ public class Inside {
                 It looks like we will soon have more rabbits!""";
 
         String[] animals = { camel, lion, deer, goose, bat, rabbit };
-        System.out.print("Please enter the number of the habitat you would like to view: ");
-        int n = Integer.valueOf(sc.nextLine());
+        boolean exit = false;
+        while (!exit) {
+            System.out.print("Please enter the number of the habitat you would like to view: ");
+            String s = sc.nextLine();
+            try {
+                int n = Integer.valueOf(s);
+                System.out.println();
+                System.out.println(animals[n]);
+            } catch (NumberFormatException nfe) {
+                System.out.println("See you later!");
+                exit = true;
+            }
+        }
         sc.close();
-        System.out.println(animals[n]);
-        System.out.println("---");
-        System.out.println(
-                "You've reached the end of the program. To check another habitat, please restart the watcher.");
     }
-
 }

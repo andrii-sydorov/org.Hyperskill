89a35ec58ee16b508083e9bae6923a794f606219
SMD_ASY <andrii.sydorov@googlemail.com>
2023-05-24 23:03:07 +0300

    The Stage02 of the project MealPlanner is prepared, but not tested.

---
+++ projects/Medium/MealPlanner/Stage02/CreateMenu.java
@@ -0,0 +1,155 @@
+package projects.Medium.MealPlanner.Stage02;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CreateMenu {
+
+    private static List<Food> meals = new ArrayList<>();
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean isRunning = true;
+        while (isRunning) {
+            System.out.println("What would you like to do (add, show, exit)?");
+            String option = sc.nextLine();
+            switch (option) {
+                case "add":
+                    addMeal(sc);
+                    System.out.println("The meal has been added!");
+                    break;
+                case "show":
+                    showMeals();
+                    break;
+                case "exit":
+                    isRunning = false;
+                default:
+                    break;
+            }
+
+        }
+        System.out.println("Bye!");
+        sc.close();
+    }
+
+    public static void addMeal(Scanner sc) {
+        System.out.println("Which meal do you want to add (breakfast, lunch, dinner)?");
+        Food f = new Food();
+        boolean isCategoryAdded = false;
+        while (!isCategoryAdded) {
+            String dish = sc.nextLine();
+            switch (dish) {
+                case "breakfast":
+                case "lunch":
+                case "dinner":
+                    f.setCategory(dish);
+                    isCategoryAdded = true;
+                    break;
+                default:
+                    System.out.println("Wrong meal category! Choose from: breakfast, lunch, dinner.");
+                    break;
+            }
+        }
+        System.out.println("Input the meal's name:");
+        boolean isNameAdded = false;
+        while (!isNameAdded) {
+            String name = sc.nextLine();
+            if (hasNoDigitsAndNotEmpty(name)) {
+                f.setName(name);
+                isNameAdded = true;
+            }
+        }
+        System.out.println("Input the ingredients:");
+        boolean isIngredientsAdded = false;
+        while (!isIngredientsAdded) {
+            String[] ingredients = sc.nextLine().split(",\\s*");
+            boolean isIngredientsCorrect = true;
+            for (String s : ingredients) {
+                if (!hasNoDigitsAndNotEmpty(s)) {
+                    isIngredientsCorrect = false;
+                    break;
+                }
+            }
+            if (isIngredientsCorrect) {
+                f.setIngredients(ingredients);
+                isIngredientsAdded = true;
+            }
+        }
+        meals.add(f);
+    }
+
+    public static void showMeals() {
+        if (meals.size() == 0) {
+            System.out.println("No meals saved. Add a meal first.");
+            return;
+        }
+        System.out.println();
+        for (Food f : meals) {
+            System.out.println(f.toString());
+            System.out.println();
+        }
+    }
+
+    public static boolean hasNoDigitsAndNotEmpty(String name) {
+        Pattern p = Pattern.compile("\\d+");
+        Matcher m = p.matcher(name);
+        if (m.find() || name.isEmpty()) {
+            System.out.println("Wrong format. Use letters only!");
+            return false;
+        }
+        return true;
+    }
+
+}
+
+class Food {
+
+    private String category;
+    private String name;
+    private String[] ingredients;
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String[] getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(String[] ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public String getListOf() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < ingredients.length; i++) {
+            if (i < ingredients.length - 1) {
+                sb.append(ingredients[i] + "\n");
+            } else {
+                sb.append(ingredients[i]);
+            }
+        }
+        return sb.toString();
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Category: %s\nName: %s\nIngredients:\n%s", category, name, getListOf());
+    }
+
+}

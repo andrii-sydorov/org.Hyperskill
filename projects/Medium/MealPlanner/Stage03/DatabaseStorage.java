705c6af507ca87c506c1bf75d98ed5e0d2f52b4e
SMD_ASY <andrii.sydorov@googlemail.com>
2023-05-29 13:19:24 +0300

    The Stage03 of the project MealPlanner was done.

--- projects/Medium/MealPlanner/Stage03/DatabaseStorage.java
+++ projects/Medium/MealPlanner/Stage03/DatabaseStorage.java
@@ -7,9 +7,173 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class DatabaseStorage {
+/**
+ * Stage 3/6: Database storage
+ * 
+ * $1. Description
+ * 
+ * At this point, when we close our program, we lose all our stored meals! Let's
+ * improve our planner and connect a database to retrieve all meals after a
+ * restart.
+ * 
+ * To connect a database to our project, we can use Java DataBase Connectivity
+ * (JDBC), an API for database-independent connectivity between programs and
+ * various databases. This standard ensures the same methods for connecting,
+ * updating, querying, and results handling, regardless of the database you
+ * employ. However, the choice of the database affects the SQL syntax, available
+ * data types, and supported features.
+ * 
+ * In this project, we will refer to PostgreSQL. It is a powerful, open source
+ * object-relational database system that contains great capabilities.
+ * PostgreSQL runs on all major operating systems and supports advanced data
+ * types such as arrays, hstore, and user-defined types. Although, there may be
+ * some differences between different SQL databases. You can find more
+ * information in the official PostgreSQL documentation
+ * 
+ * In this stage, we're going to store meal data in database tables. When the
+ * program restarts, the saved data should be available in the program.
+ * 
+ * To use PostgreSQL in Java, you need to import the postgresql-jdbc library
+ * into your project. For the Meal Planner, this is already done. You can find
+ * an example of this driver on the postgresql-jdbc website.
+ * 
+ * import java.sql.*
+ * 
+ * public class Main {
+ *   public static void main(String[] argv) throws SQLException {
+ *    String DB_URL = "jdbc:postgresql:person_db";
+ *    String USER = "postgres";
+ *    String PASS = "1111";
+ * 
+ *    Connection connection = DriverManager.getConnection(DB_URL, USER, PASS);
+ *    connection.setAutoCommit(true);
+ * 
+ *    Statement statement = connection.createStatement();
+ *    statement.executeUpdate("drop table if exists person");
+ *    statement.executeUpdate("create table person (" + "id integer," + "name varchar(1024) NOT NULL" + ")");
+ *    statement.executeUpdate("insert into person (id, name) values (1, 'leo')");
+ *    statement.executeUpdate("insert into person (id, name) values (2, 'yui')");
+ * 
+ *    ResultSet rs = statement.executeQuery("select * from person"); // Read the result set
+ *    while (rs.next()) {
+ *      System.out.println("name = " + rs.getString("name"));
+ *      System.out.println("id = " + rs.getInt("id"));
+ *    }
+ *    statement.close();
+ *    connection.close();
+ *   }
+ * }
+ * 
+ * Mind that the nested <b>resultset</b> requires different <b>statement</b> instances.
+ * 
+ * The <b>jdbc:postgresql:person_db</b> string includes three strings divided by
+ * semicolons. The first one is the database interface, the second is the
+ * database, and the third is the name of your database.
+ * 
+ * It's a good idea to use pgAdmin — a nice GUI tool for browsing and managing
+ * PostgreSQL databases. You can inspect the tables you've created and the data
+ * in your database.
+ * 
+ * If you are connected to the database file when you check your code, it may
+ * lead to issues.
+ * Make sure to create meals_db database and add postgres user with 1111
+ * password to it before initializing tests.
+ * 
+ * $2. Objectives
+ * 
+ * 1. Your program should connect to a database named <b>meals_db</b>;
+ * 2. Create two tables in this database schema. Name the first one as <b>meals</b> with
+ *    three columns: <b>category</b> (varchar), <b>meal</b> (varchar), and <b>meal_id</b> (integer).
+ *    Name the second table <b>ingredients</b>; it must contain three columns: <b>ingredient</b>
+ *    (varchar), <b>ingredient_id</b> (integer), and <b>meal_id</b> (integer). <b>meal_id</b> in both
+ *    tables must match!
+ * 3. Read all data in the tables, so their contents are available before a <b>show</b>
+ *    operation is requested;
+ * 4. When users <b>add</b> a new meal, store it in your database.
+ * 
+ * There are no changes in the input/output structure in this stage.
+ * 
+ * Note: The contents of the database tables are cleared at the beginning of the
+ * testing process.
+ * 
+ * $3. Example
+ * 
+ * The greater-than symbol followed by a space (> ) represents the user input.
+ * Note that it's not part of the input.
+ * 
+ * Example 1: standard execution and a restart
+ * 
+ * What would you like to do (add, show, exit)?
+ * > add
+ * Which meal do you want to add (breakfast, lunch, dinner)?
+ * > lunch
+ * Input the meal's name:
+ * > salad
+ * Input the ingredients:
+ * > lettuce, tomato, onion, cheese, olives
+ * The meal has been added!
+ * What would you like to do (add, show, exit)?
+ * > add
+ * Which meal do you want to add (breakfast, lunch, dinner)?
+ * > breakfast
+ * Input the meal's name:
+ * > oatmeal
+ * Input the ingredients:
+ * > oats, milk, banana, peanut butter
+ * The meal has been added!
+ * What would you like to do (add, show, exit)?
+ * > show
+ * 
+ * Category: lunch
+ * Name: salad
+ * Ingredients:
+ * lettuce
+ * tomato
+ * onion
+ * cheese
+ * olives
+ * 
+ * Category: breakfast
+ * Name: oatmeal
+ * Ingredients:
+ * oats
+ * milk
+ * banana
+ * peanut butter
+ * 
+ * What would you like to do (add, show, exit)?
+ * > exit
+ * Bye!
+ * 
+ * What would you like to do (add, show, exit)?
+ * > show
+ * 
+ * Category: lunch
+ * Name: salad
+ * Ingredients:
+ * lettuce
+ * tomato
+ * onion
+ * cheese
+ * olives
+ * 
+ * Category: breakfast
+ * Name: oatmeal
+ * Ingredients:
+ * oats
+ * milk
+ * banana
+ * peanut butter
+ * 
+ * What would you like to do (add, show, exit)?
+ * > exit
+ * Bye!
+ * 
+ * @author SMD_ASY
+ *
+ */
 
-    // private static List<Food> meals = new ArrayList<>();
+public class DatabaseStorage {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -20,7 +184,7 @@ public class DatabaseStorage {
         if (!DbUtils.createConnection(url, user, pass)) {
             System.out.println("No connection established!");
         }
-        DbUtils.deleteTable();
+        // DbUtils.deleteTable();
         DbUtils.createTables();
         while (isRunning) {
             System.out.println("What would you like to do (add, show, exit)?");
--- projects/Medium/MealPlanner/Stage03/DbUtils.java
+++ projects/Medium/MealPlanner/Stage03/DbUtils.java
@@ -15,6 +15,7 @@ public class DbUtils {
 
     private static Connection con = null;
     private static int meals_id = 1;
+    private static int ingredient_id = 1;
 
     public static boolean createConnection(String url, String user, String pass) {
         try {
@@ -43,7 +44,7 @@ public class DbUtils {
     public static void createTables() {
         try (Statement st = con.createStatement()) {
             String createMealsTable = "CREATE TABLE IF NOT EXISTS meals(category VARCHAR(20), meal VARCHAR(50), meal_id Integer);";
-            String createIngredientsTABLE = "CREATE TABLE IF NOT EXISTS ingredients(ingredient VARCHAR(70) NOT NULL, ingredient_id SERIAL, meal_id INTEGER);";
+            String createIngredientsTABLE = "CREATE TABLE IF NOT EXISTS ingredients(ingredient VARCHAR(70) NOT NULL, ingredient_id INTEGER, meal_id INTEGER);";
             st.executeUpdate(createMealsTable);
             st.executeUpdate(createIngredientsTABLE);
         } catch (SQLException sql) {
@@ -59,10 +60,13 @@ public class DbUtils {
             String addMeals = String.format("INSERT INTO meals VALUES('%s', '%s', %d);", category, name, meals_id);
             st.executeUpdate(addMeals);
             for (String s : ingredients) {
-                String addIngredients = String.format("INSERT INTO ingredients(ingredient, meal_id) VALUES('%s', %d)",
+                String addIngredients = String.format(
+                        "INSERT INTO ingredients(ingredient, ingredient_id, meal_id) VALUES('%s', %d, %d)",
                         s,
+                        ingredient_id,
                         meals_id);
                 st.executeUpdate(addIngredients);
+                ingredient_id++;
             }
         } catch (SQLException sqle) {
             System.out.println("The data are not saved in database!");
